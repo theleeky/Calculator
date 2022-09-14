2ead39eb0936902d6d9090bc0e72cc5206bb9d4d
Malek <malekbusari@gmail.com>
2022-09-14 13:33:02 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+function add(num1, num2) {
+    return num1 + num2;  
+}
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1*num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function operate(operator, num1, num2){
+    if (operator === "+"){
+        return add(num1, num2);
+    } else if (operator === "-"){
+        return subtract(num1, num2);
+    } else if (operator === "*"){
+        return multiply(num1, num2);
+    } else if (operator === "/"){
+        return divide(num1,num2);
+    }
+}
+
+console.log(operate("*", 9, 13))
