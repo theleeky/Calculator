858f147361c426c2cf4cf7aefec59818b5292bdb
Malek <malekbusari@gmail.com>
2022-09-15 14:36:20 +0100

    Small edit

--- script.js
+++ script.js
@@ -14,18 +14,23 @@ function divide(num1, num2) {
 }
 
 function operate(operator, num1, num2){
-    if (operator === "+"){
+    if (operator === "add"){
         return add(num1, num2);
-    } else if (operator === "-"){
+    } else if (operator === "subtract"){
         return subtract(num1, num2);
-    } else if (operator === "*"){
+    } else if (operator === "multiply"){
         return multiply(num1, num2);
-    } else if (operator === "/"){
+    } else if (operator === "divide"){
         return divide(num1,num2);
     }
 }
 
 let displayOne = document.querySelector('.top');
+let displayTwo = document.querySelector('.bottom')
+let typedNumber = "";
+let finalValue = 0;
+let operation = "";
+let num1 = 0;
 
 
 function btnClick() {
@@ -34,12 +39,21 @@ function btnClick() {
         selectedBtn.addEventListener('click', () => {
             // if a number is picked
             if (selectedBtn.id <= 9 || selectedBtn.id >= 0) {
-                displayOne.textContent += selectedBtn.id;
+                typedNumber += selectedBtn.id;
+                displayTwo.textContent = typedNumber;
             } else if (selectedBtn.classList.contains('operation')){
-                displayOne.textContent += " " + selectedBtn.id + " ";
-            }
+                operation = selectedBtn.id;
+                num1 = typedNumber;
+                displayOne.textContent = typedNumber + " " + operation;
+                typedNumber = "";
+                
+            } else if (selectedBtn.id === "equals"){
+                finalValue += operate(operation, finalValue, parseInt(typedNumber));
+                console.log(finalValue);
+            }   
         })
     })
 }
 
 btnClick();
+//console.log(operate("&#43;", 3,4))
