06ab8a788113e05b83c8782ef7bf0356f5d3b6a7
Malek <malekbusari@gmail.com>
2022-09-17 14:38:25 +0100

    Completed calculator functionality

--- script.js
+++ script.js
@@ -31,6 +31,18 @@ let typedNumber = "";
 let finalValue = 0;
 let operation = "";
 let num1 = 0;
+let num2 = 0;
+
+function reset(){
+    displayTwo.textContent = "";
+    displayOne.textContent = "";
+    typedNumber = "";
+    finalValue = 0;
+    operation = "";
+    num1 = 0;
+    num2 = 0
+}
+
 
 
 function btnClick() {
@@ -41,19 +53,43 @@ function btnClick() {
             if (selectedBtn.id <= 9 || selectedBtn.id >= 0) {
                 typedNumber += selectedBtn.id;
                 displayTwo.textContent = typedNumber;
+                console.log(operation.id)
+                num1 = (operation.id  == null) ? typedNumber : num1;
+                num2 = (operation.id != null) ? typedNumber : num2;
             } else if (selectedBtn.classList.contains('operation')){
-                operation = selectedBtn.id;
-                num1 = typedNumber;
-                displayOne.textContent = typedNumber + " " + operation;
+                if (num1 != 0 && num2 != 0){
+                    finalValue = operate(operation.id, parseFloat(num1), parseFloat(num2));
+                    displayTwo.textContent = finalValue;
+                    num1 = finalValue;
+                }
+                operation = selectedBtn;
+                displayOne.textContent = num1 + " " + operation.textContent;
                 typedNumber = "";
-                
-            } else if (selectedBtn.id === "equals"){
-                finalValue += operate(operation, finalValue, parseInt(typedNumber));
-                console.log(finalValue);
-            }   
+            } else if (selectedBtn.id === "equals" && num1 != 0 && num2 != 0 && operation != ""){
+                if(num1 != 0 && num2 != 0 && operation != "")
+                displayOne.textContent = num1 + " " + operation.textContent + " " + num2;
+                finalValue = operate(operation.id, parseFloat(num1), parseFloat(num2));
+                num1 = finalValue;
+                num2 = 0;
+                displayTwo.textContent = finalValue;
+            }  else if (selectedBtn.id === "AC") {
+                reset();
+            } else if (selectedBtn.id === "del") {
+                typedNumber = typedNumber.slice(0,typedNumber.length - 1);
+                num1 = (operation.id  == null) ? typedNumber : num1;
+                num2 = (operation.id != null) ? typedNumber : num2;
+                displayTwo.textContent = typedNumber;
+            } else if (selectedBtn.id === "dot") {
+                typedNumber = (typedNumber.includes(".")) ? typedNumber : (typedNumber + ".");
+                num1 = (operation.id  == null) ? typedNumber : num1;
+                num2 = (operation.id != null) ? typedNumber : num2;
+                displayTwo.textContent = typedNumber;
+            }
         })
     })
 }
 
-btnClick();
+reset();
+btnClick()
+
 //console.log(operate("&#43;", 3,4))
